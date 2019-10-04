68396d08db0d6c2f37018262778302903aa3cf41
Gray Moss <ddidwyll@gmail.com>
2019-10-04 12:55:16 +0300

    fjsdh

--- js/main.js
+++ js/main.js
@@ -8,9 +8,6 @@
 
 	'use strict';
 
-	// bootstrap dropdown hover
-
-  // loader
   var loader = function() {
     setTimeout(function() { 
       if($('#loader').length > 0) {
@@ -71,119 +68,31 @@
     }
 	});
 
-  $('.home-slider-loop-false').owlCarousel({
-    loop:false,
-    autoplay: true,
-    margin:0,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav:true,
-    autoplayHoverPause: true,
-    items: 1,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:1,
-        nav:false
-      },
-      1000:{
-        items:1,
-        nav:true
-      }
-    }
-  });
-
-	// owl carousel
-	var majorCarousel = $('.js-carousel-1');
-	majorCarousel.owlCarousel({
-    loop:true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    smartSpeed: 1000,
-    nav: true,
-    autoplayHoverPause: true,
-    items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:2,
-        nav:false
-      },
-      1000:{
-        items:3,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
-
-	// owl carousel
-	var major2Carousel = $('.js-carousel-2');
-	major2Carousel.owlCarousel({
-    loop:true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav: true,
-    autoplayHoverPause: true,
-    items: 4,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:3,
-        nav:false
-      },
-      1000:{
-        items:4,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
-
-
-  $('.centernonloop').owlCarousel({
-    center: true,
-    items: 1,
-    loop: false,
-    margin: 30,
-    smartSpeed: 1000,
-    dots: true,
-    center: true,
-    responsive:{
-      600:{
-        items: 2
-      },
-      900:{
-        items: 5
-      }
-    }
-  });
+  // $('.centernonloop').owlCarousel({
+  //   center: true,
+  //   items: 1,
+  //   loop: false,
+  //   margin: 30,
+  //   smartSpeed: 1000,
+  //   dots: false,
+  //   center: true,
+  //   responsive:{
+  //     600:{
+  //       items: 2
+  //     },
+  //     900:{
+  //       items: 5
+  //     }
+  //   }
+  // });
 
   $('.centernonloop2').owlCarousel({
     items: 1,
     loop: true,
     margin: 0,
+    dots: false,
     autoplay: true,
     smartSpeed: 1000,
-    dots: true,
   });
 
 
